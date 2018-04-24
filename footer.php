d013dba9ea25eb261fc0fc8e70e8a7fc523544ec
Wayne <wpa12@live.com>
2018-04-24 10:34:15 +0100

    just some files
    files

---
+++ footer.php
@@ -0,0 +1,7 @@
+
+<footer>
+	<?php echo "<span>Copyright &copy; ". date("Y"). " Wayne Anstey</span>"; ?>
+</footer>
+</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wayne Anstey</title>
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+<div class="container">
+<div id="header">
+	<header>
+		<img href="" />
+		<h1>Wayne Anstey</h1>
+	</header>
+</div>
