217ba38046d0e929aca010c2cc461e64873323ff
Wayne <wpa12@live.com>
2018-04-24 11:45:10 +0100

    changes

--- database.php
+++ database.php
@@ -3,13 +3,31 @@
 class database {
 
 	private $con;
-	private $file;
+	private $host, $username, $password, $dbName;
 
+	//constructor to assign the above variables to xml data 
 	function __construct () {
 		
-		$this->file = simplexml_load_file("config.xml");
+		$xml = simplexml_load_file("config.xml") or die ("Error: cannot find config.xml");
 
-		$this->con = new mysqli();
+		$this->host = $xml->host;
+		$this->username = $xml->username;
+		$this->password = $xml->password;
+		$this->dbName = $xml->dbName;
+	}
 
+	function createDatabase () {
+		$sql = "CREATE DATABASE mysite";
+		
+	}
+
+	//method to connect to the database
+	private function databaseConnect () {
+
+		$this->conn = new mysqli($this->host, $this->username, $this->password, $this->dbName) or die ("database doesn't exist");
 	}
 }
+
+$db = new database();
+
+$db->databaseConnect();
--- user.php
+++ user.php
@@ -1,10 +1,18 @@
 <?php 
 
+include_once 'database.php';
+
 class user {
 
 	public function addUser($username, $password, $email) {
 		if(!$username || !$password || !$email){
 			echo "please fill out relevant fields";
 		}
+	}
+	
+	public function loginUser($username, $password){
+
+
+		
 	}
 }
